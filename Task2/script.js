af95389144f76d951d51b0fa595b8b4a90c36415
Priti-Mishra <pm5196521@gmail.com>
2026-03-01 10:14:23 +0530

    task 2 completed

---
+++ Task2/script.js
@@ -0,0 +1,47 @@
+// Form Validation
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let name = document.getElementById("name").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let message = document.getElementById("message").value.trim();
+    let formMessage = document.getElementById("formMessage");
+
+    if (name === "" || email === "" || message === "") {
+        formMessage.style.color = "red";
+        formMessage.textContent = "All fields are required!";
+        return;
+    }
+
+    let emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if (!email.match(emailPattern)) {
+        formMessage.style.color = "red";
+        formMessage.textContent = "Enter a valid email address!";
+        return;
+    }
+
+    formMessage.style.color = "green";
+    formMessage.textContent = "Form submitted successfully!";
+});
+function addTask() {
+    let taskInput = document.getElementById("taskInput");
+    let taskText = taskInput.value.trim();
+
+    if (taskText === "") return;
+
+    let li = document.createElement("li");
+    li.textContent = taskText;
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.onclick = function() {
+        li.remove();
+    };
+
+    li.appendChild(deleteBtn);
+    document.getElementById("taskList").appendChild(li);
+
+    taskInput.value = "";
+}
